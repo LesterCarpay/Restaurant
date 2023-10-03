c4ca08bd71e470328e75aae61ce6776b86f70373
Lester Carpay <lester.carpay@gmail.com>
2023-10-02 21:04:12 -0400

    Made database behavior more modular.

--- ToDoListDB.go
+++ ToDoListDB.go
@@ -1,141 +1,74 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"Restaurant/db"
 	_ "github.com/lib/pq"
 	"log"
-	"slices"
 )
 
 type ToDoListDB struct {
-	db *sql.DB
+	db db.Database
 }
 
-func scanWithDefault(name string, defaultValue string) string {
-	fmt.Print(name + " [" + defaultValue + "]:")
-	var result string
-	_, err := fmt.Scanln(&result)
-	if err != nil || result == "" {
-		return defaultValue
-	}
-	return result
-
-}
-
-func getConnectionString() string {
-	var password string
-
-	host := scanWithDefault("Host", "localhost")
-	database := scanWithDefault("Database", "todos")
-	username := scanWithDefault("Username", "postgres")
-	fmt.Print("Password:")
-	_, err := fmt.Scanln(&password)
-	if err != nil {
-		fmt.Println("Incorrect password.")
-		log.Fatalln(err)
-	}
-	return "postgresql://" +
-		username + ":" +
-		password + "@" +
-		host + "/" +
-		database + "?sslmode=disable"
-}
+var toDosTable = db.Table{Name: "todos",
+	IDColumnName: "item_id",
+	OtherColumns: map[string]string{"item": "text"}}
 
 func (tdl *ToDoListDB) ConnectToDatabase() {
-	fmt.Println("Loading database...")
-
-	if tdl.db != nil {
-		return
-	}
-	connectionString := getConnectionString()
-	db, err := sql.Open("postgres", connectionString)
+	err := tdl.db.ConnectToDatabase()
 	if err != nil {
-		fmt.Println("Failed to initialize database")
 		log.Fatalln(err)
 	}
-	tdl.db = db
-	//tdl.GetAllItems()
-	fmt.Println("Connected to database")
 }
 
 func (tdl *ToDoListDB) CreateToDosTable() {
-	_, err := tdl.db.Exec("DROP TABLE IF EXISTS todos;" +
-		"CREATE TABLE todos (todo_id INT GENERATED ALWAYS AS IDENTITY, item text)")
+	err := tdl.db.DeleteTable(toDosTable)
 	if err != nil {
-		fmt.Println(err)
-		fmt.Println("Failed to create table.")
+		log.Fatalln(err)
+	}
+	err = tdl.db.CreateTable(toDosTable)
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
 
 func (tdl *ToDoListDB) TableExists() bool {
-	result, err := tdl.db.Query("SELECT COUNT(*) " +
-		"FROM INFORMATION_SCHEMA.TABLES " +
-		"WHERE TABLE_NAME = 'todos'")
+	result, err := tdl.db.TableExists(toDosTable)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	var rowCount int
-	for result.Next() {
-		err := result.Scan(&rowCount)
-		if rowCount < 1 || err != nil {
-			return false
-		}
-	}
-	return true
+	return result
 }
 
 func (tdl *ToDoListDB) ColumnsExist() bool {
-	result, err := tdl.db.Query("SELECT column_name " +
-		"FROM information_schema.columns " +
-		"WHERE TABLE_NAME = 'todos'")
+	result, err := tdl.db.ColumnsExist(toDosTable)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	var column string
-	var columns []string
-	for result.Next() {
-		err := result.Scan(&column)
-		if err != nil {
-			return false
-		}
-		columns = append(columns, column)
-	}
-	return slices.Contains(columns, "todo_id") &&
-		slices.Contains(columns, "item")
+	return result
 }
 
 func (tdl *ToDoListDB) AddItem(newItem string) {
-	_, err := tdl.db.Exec("INSERT into todos (item) VALUES ($1)", newItem)
+	newRow := map[string]string{"item": newItem}
+	err := tdl.db.AddRowToTable(toDosTable, newRow)
 	if err != nil {
-		fmt.Println("Failed to add item", newItem)
 		log.Fatalln(err)
 	}
 }
 
-func (tdl *ToDoListDB) DeleteItem(item_id int) {
-	_, err := tdl.db.Exec("DELETE from todos WHERE todo_id=$1", item_id)
+func (tdl *ToDoListDB) DeleteItem(itemID int) {
+	err := tdl.db.DeleteItem(toDosTable, itemID)
 	if err != nil {
-		log.Fatalln("Failed to delete item.")
+		log.Fatalln(err)
 	}
 }
 
 func (tdl *ToDoListDB) getColumnValues(column string) []string {
-	var item string
-	var items []string
-
-	rows, err := tdl.db.Query(fmt.Sprintf("SELECT %v FROM todos", column))
+	values, err := tdl.db.GetColumnValues(toDosTable, column)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	for rows.Next() {
-		err := rows.Scan(&item)
-		if err != nil {
-			print(err)
-		}
-		items = append(items, item)
-	}
-	return items
+	return values
 }
 
 func (tdl *ToDoListDB) GetAllItems() []string {
@@ -143,7 +76,7 @@ func (tdl *ToDoListDB) GetAllItems() []string {
 }
 
 func (tdl *ToDoListDB) GetIDs() []string {
-	return tdl.getColumnValues("todo_id")
+	return tdl.getColumnValues(toDosTable.IDColumnName)
 }
 
 func (tdl *ToDoListDB) GetItem(i int) string {
---
+++ db/database.go
@@ -0,0 +1,172 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"slices"
+)
+
+type Table struct {
+	Name         string
+	IDColumnName string
+	OtherColumns map[string]string
+}
+
+type Database struct {
+	tables []Table
+	sqlDB  *sql.DB
+}
+
+func scanWithDefault(name string, defaultValue string) string {
+	fmt.Print(name + " [" + defaultValue + "]:")
+	var result string
+	_, err := fmt.Scanln(&result)
+	if err != nil || result == "" {
+		return defaultValue
+	}
+	return result
+}
+
+func getConnectionString() string {
+	var password string
+
+	host := scanWithDefault("Host", "localhost")
+	dbName := scanWithDefault("Database", "todos")
+	username := scanWithDefault("Username", "postgres")
+	fmt.Print("Password:")
+	_, err := fmt.Scanln(&password)
+	if err != nil {
+		fmt.Println("Incorrect password.")
+		log.Fatalln(err)
+	}
+	return "postgresql://" +
+		username + ":" +
+		password + "@" +
+		host + "/" +
+		dbName + "?sslmode=disable"
+}
+
+func (db *Database) ConnectToDatabase() error {
+	fmt.Println("Loading Database...")
+
+	connectionString := getConnectionString()
+	sqlDB, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		return err
+	}
+	db.sqlDB = sqlDB
+	//tdl.GetAllItems()
+	fmt.Println("Connected to Database")
+	return nil
+}
+
+func (db *Database) DeleteTable(table Table) error {
+	_, err := db.sqlDB.Exec(fmt.Sprintf("DROP TABLE IF EXISTS %v", table.Name))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (db *Database) CreateTable(table Table) error {
+	_, err := db.sqlDB.Exec(fmt.Sprintf("CREATE TABLE %v "+
+		"(%v INT GENERATED ALWAYS AS IDENTITY)", table.Name, table.IDColumnName))
+	if err != nil {
+		return err
+	}
+	for colName, colType := range table.OtherColumns {
+		_, err = db.sqlDB.Exec(fmt.Sprintf("ALTER TABLE %v ADD %v %v",
+			table.Name, colName, colType))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (db *Database) TableExists(table Table) (bool, error) {
+	result, err := db.sqlDB.Query(fmt.Sprintf(
+		"SELECT COUNT(*) "+
+			"FROM INFORMATION_SCHEMA.TABLES "+
+			"WHERE TABLE_NAME = '%v'", table.Name))
+	if err != nil {
+		return false, err
+	}
+	var rowCount int
+	for result.Next() {
+		err := result.Scan(&rowCount)
+		if err != nil {
+			return false, err
+		}
+		if rowCount < 1 {
+			return false, nil
+		}
+	}
+	return true, nil
+}
+
+func (db *Database) ColumnsExist(table Table) (bool, error) {
+	result, err := db.sqlDB.Query("SELECT column_name " +
+		"FROM information_schema.columns " +
+		"WHERE TABLE_NAME = 'todos'")
+	if err != nil {
+		return false, err
+	}
+	var column string
+	var columns []string
+	for result.Next() {
+		err := result.Scan(&column)
+		if err != nil {
+			return false, err
+		}
+		columns = append(columns, column)
+	}
+	for colName := range table.OtherColumns {
+		if !slices.Contains(columns, colName) {
+			return false, nil
+		}
+	}
+	if !slices.Contains(columns, table.IDColumnName) {
+		return false, nil
+	}
+	return true, nil
+}
+
+func (db *Database) AddRowToTable(table Table, newRow map[string]string) error {
+	colString := "("
+	valString := "("
+	for colName, value := range newRow {
+		colString += colName + ","
+		valString += fmt.Sprintf("'%v',", value)
+	}
+	colString = colString[:len(colString)-1] + ")"
+	valString = valString[:len(valString)-1] + ")"
+	_, err := db.sqlDB.Exec(fmt.Sprintf("INSERT into %v %v VALUES %v",
+		table.Name, colString, valString))
+	return err
+}
+
+func (db *Database) DeleteItem(table Table, id int) error {
+	_, err := db.sqlDB.Exec(fmt.Sprintf("DELETE from %v WHERE %v=%v",
+		table.Name, table.IDColumnName, id))
+	return err
+}
+
+func (db *Database) GetColumnValues(table Table, column string) ([]string, error) {
+	var item string
+	var items []string
+
+	rows, err := db.sqlDB.Query(fmt.Sprintf("SELECT %v FROM %v", column, table.Name))
+	if err != nil {
+		return nil, err
+	}
+	for rows.Next() {
+		err := rows.Scan(&item)
+		if err != nil {
+			return nil, err
+		}
+		items = append(items, item)
+	}
+	return items, nil
+}
